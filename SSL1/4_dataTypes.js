32681012d6fc8f9aa0ded7aad23bd68161f7497e
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-03 14:29:55 +0530

    concatenation and type coercion

---
+++ SSL1/4_dataTypes.js
@@ -0,0 +1,20 @@
+// number null undefined biginit boolean string symbol
+
+let a = 12; // number
+let c = null; //  null means this variable is intentionally is null
+let notIntialized; // undefined
+let d = true; //  boolean
+let naam = "Luffy"; // string
+
+console.log(naam);
+console.log(typeof naam);
+
+console.log("2" + 2); // internal conversion when its +
+console.log(2 + "2");
+console.log("2" - 2);
+console.log(2 - "2");
+
+let firstName = "watch";
+let lastName = "Here";
+console.log(firstName + lastName);
+console.log(firstName + " " + lastName);
