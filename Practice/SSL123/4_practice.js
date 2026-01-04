2cf70dbac00ef657d659717e8c9f70f7cda04760
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-04 20:33:52 +0530

    small practices

---
+++ Practice/SSL123/4_practice.js
@@ -0,0 +1,5 @@
+const num1 = Number(prompt("Enter a First Number: "));
+const num2 = Number(prompt("Enter a Second Number: "));
+
+const result = num1 + num2;
+console.log("Addition Result: ", result);
