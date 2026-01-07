6129c8adb8c5c283ea414118f5a3d0d0ad41b979
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-07 19:49:23 +0530

    practice

---
+++ Practice/SSL4/5_practice.js
@@ -0,0 +1,7 @@
+//choice to select theme color
+const themeColor = "red";
+if (themeColor) {
+  console.log(`${themeColor} theme color is selected`);
+} else {
+  console.log(`No theme color is selected`);
+}
